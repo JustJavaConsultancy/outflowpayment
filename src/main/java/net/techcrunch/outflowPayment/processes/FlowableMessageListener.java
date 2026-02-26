dcf06d09fc19bf9b599cb6043f47c9f704a751af
mrpelumi <oguntolapelumi6@gmail.com>
2026-02-26 10:19:36 +0100

    conditional logic for handlepaymentmessage edited

--- src/main/java/net/techcrunch/outflowPayment/processes/FlowableMessageListener.java
+++ src/main/java/net/techcrunch/outflowPayment/processes/FlowableMessageListener.java
@@ -31,7 +31,7 @@ public class FlowableMessageListener {
             Map<String, Object> transferDTO = mapper.convertValue(variable.get("TransferDTO"), Map.class);
 //            if (transferDTO.get("recipientName") != null)
 //                transferDTO.put("beneficiaryName", transferDTO.get("recipientName"));
-            if (transferDTO.get("duration") != null){
+            if (transferDTO.get("duration") != null && transferDTO.get("duration") != "Select"){
                 DurationType period = DurationType.valueOf(transferDTO.get("duration").toString().toUpperCase());
                 LocalDate nextDate = period.nextBillingDate(LocalDate.now());
                 System.out.println("nextBillingDate = " + nextDate);
